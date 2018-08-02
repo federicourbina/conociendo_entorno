3b5fbd053a35d4e02aa8dae484a7172061a84fc4
federico.urbina <federico.urbina@prepaenlinea.sep.gob.mx>
2018-08-02 13:44:55 -0500

    prueba

--- saeta_user(2).sql
+++ saeta_user(2).sql
@@ -1,10 +1,10 @@
--- phpMyAdmin SQL Dump
+﻿-- phpMyAdmin SQL Dump
 -- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
 -- Tiempo de generación: 02-08-2018 a las 01:42:03
--- Versión del servidor: 10.1.30-MariaDB
+-- V: 10.1.30-MariaDB
 -- Versión de PHP: 7.1.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
